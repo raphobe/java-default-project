71d640213909cfc16812ea850febf81ef3846740
Matthew Martinez <mmartinez@prog02.var.martinezmanor.com>
2021-02-20 20:45:50 -0500

    utilizing correct path for moving file

--- bin/rename_quickstart.sh
+++ bin/rename_quickstart.sh
@@ -5,5 +5,5 @@ name="$1"
 grep -R QuickStart src | cut -f1 -d':' | sort | uniq | xargs -I{} sh -c "sed -i 's/QuickStart/$name/g' {}"
 
 git mv src/test/java/be/rapho/TestQuickStart.java src/test/java/be/rapho/Test$name.java
-git mv src/main/java/be/rapho/QuickStart.java src/test/java/be/rapho/$name.java
+git mv src/main/java/be/rapho/QuickStart.java src/main/java/be/rapho/$name.java
 
