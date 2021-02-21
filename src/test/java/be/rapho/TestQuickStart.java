b53be15e5f68f9a74c236f20dc621fcd2ce82a6a
Matthew Martinez <mmartinez@prog02.var.martinezmanor.com>
2021-02-20 19:55:22 -0500

    initial skeleton of java tests

---
+++ src/test/java/be/rapho/TestQuickStart.java
@@ -0,0 +1,17 @@
+package be.rapho;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+public class TestQuickStart {
+
+  @Test
+  public void testNoArgument() throws Exception {
+      assertEquals(1, 1);
+  }
+}
